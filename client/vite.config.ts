40a0e93f2bca84cb781abfd7206753b0dc839cac
Souvik Pan <souvikpan2006@gmail.com>
2026-07-31 21:29:30 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   },
-})
+  server: {
+    port: 3000
+  }
+});
