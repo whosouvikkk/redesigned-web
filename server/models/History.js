c4066b3a7f856e3640efd997cde23dae453c7771
Souvik Pan <souvikpan2006@gmail.com>
2026-07-31 21:51:29 +0530

    Update History.js

--- server/models/History.js
+++ server/models/History.js
@@ -4,7 +4,7 @@ const historySchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   type: { type: String, required: true },
   query: { type: String, required: true },
-  status: { type: String, enum: ['success', 'failed'], default: 'success' }
+  status: { type: String, enum: ['success', 'failed'], required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('History', historySchema);
